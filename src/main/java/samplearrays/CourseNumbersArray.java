fc996b2da2e751b5b36c77f54d5b7ef61c773793
Abdelilah BAAMOUDI <abd.baamoudi@gmail.com>
2026-09-23 22:55:04 +0000

    Exercise 1: implement addNewCourse, printUpdatedCourses, checkCourses

--- src/main/java/samplearrays/CourseNumbersArray.java
+++ src/main/java/samplearrays/CourseNumbersArray.java
@@ -1,8 +1,37 @@
 package samplearrays;
 
+
+import java.util.Arrays;
+
 public class CourseNumbersArray {
+    public static int[] addNewCourse(int[] courses,int newCourse){
+        int[] updatedCourses= Arrays.copyOf(courses,courses.length+1);
+        updatedCourses[courses.length]=newCourse;
+        return updatedCourses;
+
+    }
+    public static void printUpdatedCourses(int[] updatedCourses){
+        System.out.println(Arrays.toString(updatedCourses));
+
+    }
+    public static boolean checkCourses(int[] courses,int course){
+        for(int cour : courses){
+            if( cour==course)
+                return true;
+        }
+        return false;
+    }
+
+
+
     public static void main(String[] args) {
         int[] registeredCourses = {1010, 1020, 2080, 2140, 2150, 2160};
+
+        int[] updatedCourses=CourseNumbersArray.addNewCourse(registeredCourses,3090);
+        CourseNumbersArray.printUpdatedCourses(updatedCourses);
+        System.out.println(CourseNumbersArray.checkCourses(registeredCourses,1020));
+        System.out.println(CourseNumbersArray.checkCourses(registeredCourses,9999));
+
 
     }
 }
