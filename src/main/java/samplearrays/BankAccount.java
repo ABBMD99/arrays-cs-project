30f4659e99145fd6533da014c8eb6a88c99a8598
Abdelilah BAAMOUDI <abd.baamoudi@gmail.com>
2026-09-23 23:54:12 +0000

    Exercise 3: Implement the class BankAccount and its 4 required methods.

--- src/main/java/samplearrays/BankAccount.java
+++ src/main/java/samplearrays/BankAccount.java
@@ -2,27 +2,52 @@ package samplearrays;
 
 public class BankAccount {
 
-    String name;
-    double currentBalance;
+    private String name;
+    private double currentBalance;
     //TO-DO: Initialize an Array with 1000 in size that stores Double called 'transactions' to keep track of the user's transactions
+    private double[] transactions=new double[1000];
+    private  int i=0;
 
-    public BankAccount(String name, int startingBalance){
-
+    public BankAccount(String name, double  startingBalance){
+        this.name=name;
+        this.currentBalance=startingBalance;
     }
 
     public void deposit(double amount){
-
+        if(amount<=0){
+            System.out.println("Sorry ! The amount must be positive ");
+            return;
+        }
+        this.currentBalance+=amount;
+        transactions[i]=amount;
+        i++;
+        System.out.println("Name: " + this.name + "\nDeposited amount: " + amount + "\nNew Balance: " + this.currentBalance);
     }
 
     public void withdraw(double amount){
+        if(amount <= 0){
+            System.out.println("Sorry! The amount must be positive.");
+            return;
+        }
+        if(amount>this.currentBalance ){
+            System.out.println("Withdrawal was unsuccessful (You do not have enough money!)");
+            return;
+        }
+        this.currentBalance-=amount;
+        transactions[i]=-amount;
+        i++;
 
     }
 
     public void displayTransactions(){
+        for(int j=0;j<this.i;j++){
+            System.out.println((j+1)+": "+this.transactions[j]);
+        }
 
     }
 
     public void displayBalance(){
+        System.out.println("Your current balance is: "+this.currentBalance);
 
     }
 
