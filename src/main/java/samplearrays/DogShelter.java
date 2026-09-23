3f74db8ff8baef3411f81421d780ade80f7b050a
Abdelilah BAAMOUDI <abd.baamoudi@gmail.com>
2026-09-23 23:27:36 +0000

    Exercise 2: fix DogShelter array declaration and implement methods.

--- src/main/java/samplearrays/DogShelter.java
+++ src/main/java/samplearrays/DogShelter.java
@@ -3,14 +3,18 @@ package samplearrays;
 public class DogShelter {
 
     // initialize an array of 3
+    private static final int[] dogCounts=new int[3];
 
 
     public static void main(String[] args) {
 
         // Adding counts for three types of dogs
-        dogCounts[0] = 15;
-        dogCounts[1] = 30;
-        dogCounts[2] = 20;
+        //dogCounts[0] = 15;
+        //dogCounts[1] = 30;
+        //dogCounts[2] = 20;
+        addBreed(0,15);
+        addBreed(1,30);
+        addBreed(2,20);
 
         // Display initial dog counts
         System.out.println("Initial Dog Counts:");
@@ -30,17 +34,30 @@ public class DogShelter {
     // Add count to a given index
     public static void addBreed(int index, int count) {
         // add you code here
+        if(index>dogCounts.length-1||index <0){
+            System.out.println("index out of range");
+            return;
+        }
+        dogCounts[index]+=count;
     }
 
     // Remove a breed by setting its count to 0
     public static void deleteBreed(int index) {
         // add your code here
+        if (index > dogCounts.length - 1 || index < 0) {
+            System.out.println("index out of range");
+            return;
+        }
+        dogCounts[index]=0;
     }
 
     // Display all dog counts
     public static void displayDogs() {
         // add your code
-          //  System.out.println("Breed " + i + " has " + dogCounts[i] + " dogs.");
+        for(int i=0;i<dogCounts.length;i++){
+            System.out.println("Breed " + i + " has " + dogCounts[i] + " dogs.");
+        }
+
 
     }
 }
