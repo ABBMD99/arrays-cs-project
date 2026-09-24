4b36a9c7c3afd5b25a243293d3044f1920a8b0f1
Abdelilah BAAMOUDI <abd.baamoudi@gmail.com>
2026-09-24 20:05:34 +0000

    Exercise 4: Version 2 (Not done yet still need some refinements!!)

--- src/main/java/samplearrays/ManageStudent.java
+++ src/main/java/samplearrays/ManageStudent.java
@@ -55,26 +55,65 @@ public class ManageStudent {
 
     // 6) Sort Students by Grade (descending)
     public static void sortByGradeDesc(Student[] students) {
+        //Bubble sort:
 
+        for (int i = students.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (students[j].getGrade() < students[j + 1].getGrade()) {
+                    Student temp = students[j];
+                    students[j] = students[j + 1];
+                    students[j + 1] = temp;
+                }
+
+            }
+
+
+        }
     }
 
     // 7) Print High Achievers (grade >= 15)
     public static void printHighAchievers(Student[] students) {
+        for(Student s:students){
+            if(s.getGrade()>=15){
+                System.out.println(s.getName());
+            }
+        }
 
     }
 
     // 8) Update Student Grade by id
     public static boolean updateGrade(Student[] students, int id, int newGrade) {
+        for(Student s:students){
+            if(s.getId()==id){
+                s.setGrade(newGrade);
+                return true;
+            }
+        }
+        return false;
 
     }
 
     // 9) Find Duplicate Names
     public static boolean hasDuplicateNames(Student[] students) {
+        for(int i=0;i<students.length;i++){
+            for(int j=i+1;j< students.length;j++){
+                if(students[i].getName().equals(students[j].getName())){
+                    return true;
+                }
+            }
+        }
+        return false;
 
     }
 
     // 10) Expandable Array: return a new array with one more slot and append student
     public static Student[] appendStudent(Student[] students, Student newStudent) {
+        Student[] newStudents=new Student[students.length+1];
+        for(int i=0;i<students.length;i++){
+            newStudents[i]=students[i];
+        }
+        newStudents[students.length]=newStudent;
+        return newStudents;
 
     }
 
@@ -87,7 +126,7 @@ public class ManageStudent {
         arr[1] = new Student(2, "Sara", 20);
         arr[2] = new Student(3, "Yassmin", 19, 16);
         arr[3] = new Student(4, "Hamza", 17, 14);
-        arr[4] = new Student(5, "Abdelilah", 20, 1);
+        arr[4] = new Student(5, "Abdelilah", 20, 20);
 
 
         // Print all
@@ -96,15 +135,24 @@ public class ManageStudent {
         System.out.println("Total created: " + Student.getNumStudent());
 
         // 2) Oldest
+        System.out.println("The oldest");
+        System.out.println(ManageStudent.findOldest(arr));
 
 
         // 3) Count adults
+        System.out.println("The Adults");
+        System.out.println(ManageStudent.countAdults(arr));
 
 
         // 4) Average grade
+        System.out.println("Average");
+        System.out.println(ManageStudent.averageGrade(arr));
+
 
 
         // 5) Find by name
+        System.out.println("Searching : Yassmin?");
+        System.out.println(ManageStudent.findStudentByName(arr,"Yassmin"));
 
 
         // 6) Sort by grade desc
@@ -118,13 +166,45 @@ public class ManageStudent {
 
         // 8) Update grade by id
         // function
-        System.out.println("\nUpdated id=4? " + updated);
-        System.out.println(findStudentByName(arr, "Dina"));
+        //System.out.println("\nUpdated id=4? " + updated);
+        //System.out.println(findStudentByName(arr, "Dina"));
 
         // 9) Duplicate names
 
 
         // 10) Append new student
+
+
+        // 11)  Student Matrix
+
+        Student[][] classes=new Student[2][3];
+
+        classes[0][0] = new Student(11, "Ali");
+        classes[0][1] = new Student(22, "Sara", 20);
+        classes[0][2] = new Student(33, "Yassmin", 19, 16);
+
+        classes[1][0] = new Student(44, "Hamza", 17, 14);
+        classes[1][1] = new Student(55, "Abdelilah", 20, 20);
+        classes[1][2] = new Student(66, "Nora", 18, 17);
+        System.out.println("--Print the names of all students class by class");
+
+        for(int i=0;i<2;i++){
+            System.out.println("__Class: "+(i+1)+"__");
+            for(int j=0;j<3;j++){
+                System.out.println(classes[i][j].getName());
+            }
+        }
+
+        System.out.println("The Top Student in Each Class");
+        sortByGradeDesc(classes[0]);
+        System.out.println("Class 1 :");
+        System.out.println(classes[0][0]);
+        sortByGradeDesc(classes[1]);
+        System.out.println("Class 2 :");
+        System.out.println(classes[1][0]);
+
+
+
 
     }
 }
